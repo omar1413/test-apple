43b7712d78527eecb3ca69d75798971144aa8dfc
Omar Osama <omar.osama@sap.com>
2024-05-29 15:12:28 +0300

    first commit

---
+++ src/test/java/com/example/apple/AppleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
