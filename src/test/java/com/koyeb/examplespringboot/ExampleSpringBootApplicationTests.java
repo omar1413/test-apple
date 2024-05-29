6255570c367a54e2e56284267d8bc23b739bcb10
Omar Osama <omar.osama@sap.com>
2024-05-29 15:26:27 +0300

    test

--- src/test/java/com/koyeb/examplespringboot/ExampleSpringBootApplicationTests.java
+++ src/test/java/com/koyeb/examplespringboot/ExampleSpringBootApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.apple;
+package com.koyeb.examplespringboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AppleApplicationTests {
+class ExampleSpringBootApplicationTests {
 
-  @Test
-  void contextLoads() {
-  }
+	@Test
+	void contextLoads() {
+	}
 
 }
